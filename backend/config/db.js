929746310730a624bb17c4aef52066ce36046317
sayyed-sprinter <sprintersagile@gmail.com>
2021-07-06 16:31:51 +0545

    updated server.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose')
-try{
-  mongoose.connect("mongodb+srv://sprintersagile:the-sprinters@motortaxapp.zndgq.mongodb.net/motortaxdatabase?retryWrites=true&w=majority",{
-    useNewUrlParser: true, 
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(process.env.NODE_ENV);
+
+const mongoose = require('mongoose');
+try {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology : true
-})
-console.log(
-  `MongoDB connected`
-);
-}
-catch(err){
-  console.error(`Error: ${err}`)
+    useUnifiedTopology: true,
+  });
+  console.log(`MongoDB connected`);
+} catch (err) {
+  console.error(`Error: ${err}`);
 }
--- backend/routes/vehicle.js
+++ backend/routes/vehicle.js
@@ -1,8 +1,8 @@
-const express =require('express'); 
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
 const Vehicle = require('../models/vehicleModel');
-const vehicleController= require('../controller/vehicleController')
+const vehicleController = require('../controllers/vehicleController');
 
-router.post('/vehicle/payTax',vehicleController) 
+router.post('/vehicle/payTax', vehicleController);
 
-module.exports=router;
+module.exports = router;
