d798a9c0b7de45440be42e8fbd7e3be676b2302f
sayyed-sprinter <sprintersagile@gmail.com>
2021-07-06 17:34:28 +0545

    Deleting backend files

--- backend/routes/vehicle.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Vehicle = require('../models/vehicleModel');
-const vehicleController = require('../controllers/vehicleController');
-
-router.post('/vehicle/payTax', vehicleController);
-
-module.exports = router;
