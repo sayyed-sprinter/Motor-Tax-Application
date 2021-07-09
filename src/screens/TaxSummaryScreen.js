6cda17ab92ed88743afac360fdc840044b805992
sayyed-sprinter <sprintersagile@gmail.com>
2021-07-09 15:35:44 +0545

    Added download statement function

--- src/screens/TaxSummaryScreen.js
+++ src/screens/TaxSummaryScreen.js
@@ -1,16 +1,55 @@
 import React from 'react';
+
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { VscLoading } from 'react-icons/vsc';
+import pdfMake from 'pdfmake/build/pdfmake';
+
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+
 import Button from '../components/Button';
 import List from '../components/List';
 import { FaFilePdf } from 'react-icons/fa';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const TaxSummaryScreen = ({ history }) => {
   const record = useSelector((state) => state.taxpayer);
   const { loading, error, taxpayerinfo } = record;
 
-  console.log(taxpayerinfo);
+  const downloadClickListener = () => {
+    var docDefinition = {
+      content: [
+        { text: `${taxpayerinfo.taxpayer_name}`, style: 'header' },
+        { text: '\n\nVehicle Details', style: 'header' },
+        {
+          ul: [
+            `Bluebook Number: ${taxpayerinfo.bluebook_number}`,
+            `Vehicle Number: ${taxpayerinfo.vehicle_number}`,
+            `Vehicle Type: ${taxpayerinfo.type}`,
+            `Engine CC: ${taxpayerinfo.engine_cc}`,
+            `Province: ${taxpayerinfo.province}`,
+          ],
+        },
+        { text: '\nTax Charges', style: 'header' },
+        {
+          ul: [
+            `Tax Amount: NPR.${taxpayerinfo.taxAmount}`,
+            `Tax overdue period: ${taxpayerinfo.taxOverdue}`,
+            `Penalty on overdue: NPR.${taxpayerinfo.penaltyOnOverdue}`,
+            `Polluting vehicle charge: NPR.${taxpayerinfo.pollutingCharge}`,
+            `Total amount to pay: NPR.${
+              taxpayerinfo.taxAmount +
+              taxpayerinfo.penaltyOnOverdue +
+              taxpayerinfo.pollutingCharge
+            }`,
+          ],
+        },
+      ],
+    };
+
+    pdfMake.createPdf(docDefinition).download();
+  };
 
   return (
     <>
@@ -24,9 +63,12 @@ const TaxSummaryScreen = ({ history }) => {
           <div className='payment-details'>
             <div className='taxpayer-details'>
               <h2 className='taxpayer'>{taxpayerinfo.taxpayer_name}</h2>
-              <Link to='' className='download-taxpayer-details'>
-                <FaFilePdf className='pdf-icon' />
-              </Link>
+              <div className='download-taxpayer-details'>
+                <FaFilePdf
+                  className='pdf-icon'
+                  onClick={downloadClickListener}
+                />
+              </div>
             </div>
             <List
               title='Vehicle details'
@@ -45,7 +87,7 @@ const TaxSummaryScreen = ({ history }) => {
               title='Tax charges'
               key1='Tax amount'
               value1={`NPR. ${taxpayerinfo.taxAmount}`}
-              key2='Tax overdue'
+              key2='Tax overdue period'
               value2={`${taxpayerinfo.taxOverdue} ${
                 taxpayerinfo.taxOverdue > 1 ? 'years' : 'year'
               }`}
