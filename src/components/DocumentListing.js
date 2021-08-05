c24b0d5e0cd79e2f57c56284dfeb9e22b171dd85
sayyed-sprinter <sprintersagile@gmail.com>
2021-08-05 19:36:47 +0545

    Added admin scren for Verify Insurance Company function

--- src/components/DocumentListing.js
+++ src/components/DocumentListing.js
@@ -9,8 +9,9 @@ import {
 } from '../actions/adminDocsAction';
 import Loader from './Loader';
 import MessageBar from './MessageBar';
+import { getAllInsuranceCompanies } from '../actions/insuranceAction';
 
-const DocumentListing = ({ history }) => {
+const DocumentListing = ({ taxpayer, insuranceCompany }) => {
   const dispatch = useDispatch();
 
   const [btnCheckClicked, setBtnCheckedClicked] = useState(false);
@@ -18,6 +19,13 @@ const DocumentListing = ({ history }) => {
 
   const adminDocuments = useSelector((state) => state.adminDocs);
   const { loading, error, adminDocs } = adminDocuments;
+
+  const insComapnies = useSelector((state) => state.insurance);
+  const {
+    insuranceCompanies,
+    loading: loadingInsCompanies,
+    error: errorInsCompanies,
+  } = insComapnies;
 
   const updatedDocs = useSelector((state) => state.adminUpdatesTaxpayer);
   const { loading: updateLoading, success } = updatedDocs;
@@ -32,6 +40,7 @@ const DocumentListing = ({ history }) => {
       }
       setBtnCheckedClicked(false);
     },
+
     [dispatch, btnCheckClicked, updateLoading, success]
   );
 
@@ -47,6 +56,8 @@ const DocumentListing = ({ history }) => {
 
   useEffect(() => {
     dispatch(getAllAdminDocs());
+
+    dispatch(getAllInsuranceCompanies());
     if (success) dispatch(getAllAdminDocs());
   }, [dispatch, success]);
 
@@ -57,9 +68,14 @@ const DocumentListing = ({ history }) => {
       ) : error ? (
         <MessageBar text={error} error={true} />
       ) : (
+        taxpayer &&
         adminDocs &&
         adminDocs.map((item, index) => (
-          <section className='docs-list' key={`docs-${index}`}>
+          <section
+            className='docs-list'
+            key={`docs-${index}`}
+            id='admin-taxpayers-docs'
+          >
             <h3 className='heading-3 main-heading taxpayer-name'>
               {item.taxpayer}
             </h3>
@@ -71,31 +87,139 @@ const DocumentListing = ({ history }) => {
                   <p className='doc'>
                     <span>Bluebook:</span>{' '}
                     <a
-                      href={doc.bluebook_file_path}
+                      href={
+                        doc.bluebook_file_path ? doc.bluebook_file_path : '#'
+                      }
                       target='_blank'
                       rel='noreferrer'
                     >
-                      {doc.bluebook_file_path.split('/')[4]}
+                      {doc.bluebook_file_path &&
+                        doc.bluebook_file_path.split('/')[4]}
                     </a>
                   </p>
                   <p className='doc'>
                     <span>Citizenship:</span>{' '}
                     <a
-                      href={doc.citizenship_file_path}
+                      href={
+                        doc.citizenship_file_path
+                          ? doc.citizenship_file_path
+                          : '#'
+                      }
                       target='_blank'
                       rel='noreferrer'
                     >
-                      {doc.citizenship_file_path.split('/')[4]}
+                      {doc.citizenship_file_path &&
+                        doc.citizenship_file_path.split('/')[4]}
                     </a>
                   </p>
                   <p className='doc'>
                     <span>Policy:</span>{' '}
                     <a
-                      href={doc.policy_file_path}
+                      href={doc.policy_file_path ? doc.policy_file_path : '#'}
                       target='_blank'
                       rel='noreferrer'
                     >
-                      {doc.policy_file_path.split('/')[4]}
+                      {doc.policy_file_path &&
+                        doc.policy_file_path.split('/')[4]}
+                    </a>
+                  </p>
+                </section>
+              ))}
+
+              <section className='btn-container'>
+                {!item.verified ? (
+                  !item.adminComment ? (
+                    <>
+                      <p
+                        id={`btn-verify-${index}`}
+                        className='btn btn--success'
+                        onClick={(e) =>
+                          verifiedClickHandler({
+                            _id: item._id,
+                            verified: true,
+                          })
+                        }
+                      >
+                        <MdCheck />
+                      </p>
+                      <p
+                        id={`btn-error-verify-${index}`}
+                        className='btn btn--error'
+                        onClick={(e) =>
+                          unVerifiedClickHandler({
+                            _id: item._id,
+                            verified: false,
+                            adminComment: 'Invalid documents!!',
+                          })
+                        }
+                      >
+                        <MdClose />
+                      </p>
+                    </>
+                  ) : (
+                    <p
+                      className='btn--error admin-comment'
+                      id={`admin-verification-comment-${index}`}
+                    >
+                      {item.adminComment}
+                    </p>
+                  )
+                ) : (
+                  <p
+                    className='btn--success admin-comment'
+                    id={`doc-verified-${index}`}
+                  >
+                    verified
+                  </p>
+                )}
+              </section>
+            </section>
+          </section>
+        ))
+      )}
+      {loadingInsCompanies ? (
+        <Loader />
+      ) : errorInsCompanies ? (
+        <MessageBar text={error} error={true} />
+      ) : (
+        insuranceCompany &&
+        insuranceCompanies &&
+        insuranceCompanies.map((item, index) => (
+          <section
+            className='docs-list'
+            key={`docs-${index}`}
+            id='admin-insurance-companies-docs'
+          >
+            <h3 className='heading-3 main-heading taxpayer-name'>
+              {item.insurance_company}
+            </h3>
+
+            <section className='docs-details'>
+              <p className='docs-date'>{item.createdAt.split('T')[0]}</p>
+              {item.docs.map((doc, index) => (
+                <section className='docs' key={`doc-${index}`}>
+                  <p className='doc'>
+                    <span>Documents</span>{' '}
+                  </p>
+                  <p className='doc'>
+                    <span>VAT:</span>{' '}
+                    <a
+                      href={doc.vat_file_path ? doc.vat_file_path : '#'}
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      {doc.vat_file_path && doc.vat_file_path.split('/')[4]}
+                    </a>
+                  </p>
+                  <p className='doc'>
+                    <span>License:</span>{' '}
+                    <a
+                      href={doc.license_file_path ? doc.license_file_path : '#'}
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      {doc.license_file_path &&
+                        doc.license_file_path.split('/')[4]}
                     </a>
                   </p>
                 </section>
--- src/screens/AdminScreen.js
+++ src/screens/AdminScreen.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DocumentListing from '../components/DocumentListing';
 
 const AdminScreen = ({ history }) => {
-  return (
-    <section className='admin-container'>
-      <h1 className='heading-1 taxpayer-docs' id='taxpayer-documents'>
-        Taxpayer Documents
-      </h1>
+  const [taxpayer, setTaxpayer] = useState(true);
+  const [insuranceCompany, setInsuranceCompany] = useState(false);
 
-      <DocumentListing history={history} />
-    </section>
+  return (
+    <>
+      <section className='admin-container'>
+        <section className='nav-admin-container' id='nav-admin-container'>
+          <section className={`nav-admin nav-Taxpayer`}>
+            <p
+              id={`menu-Taxpayer`}
+              onClick={() => {
+                setTaxpayer(true);
+                setInsuranceCompany(false);
+              }}
+            >
+              Taxpayer
+            </p>
+          </section>
+          <section className={`nav-admin nav-insurance-company`}>
+            <p
+              id={`menu-insurance-company`}
+              onClick={() => {
+                setTaxpayer(false);
+                setInsuranceCompany(true);
+              }}
+            >
+              Insurance Company
+            </p>
+          </section>
+        </section>
+        <h1 className='heading-1 taxpayer-docs' id='taxpayer-documents'>
+          {taxpayer && 'Taxpayer Documents'}
+          {insuranceCompany && 'Insurance Company Documents'}
+        </h1>
+
+        {taxpayer && <DocumentListing history={history} taxpayer={taxpayer} />}
+        {insuranceCompany && (
+          <DocumentListing
+            history={history}
+            insuranceCompany={insuranceCompany}
+          />
+        )}
+      </section>
+    </>
   );
 };
 
